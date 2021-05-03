45a3108379ac607cfb88578462afe216a6782c68
marcos-dev-web <marcos.dev.web.frontend@gmail.com>
2021-05-03 07:25:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Load from './components/Load';
-
-const Home = lazy(() => import('./pages/Home'));
-const AllPosts = lazy(() => import('./pages/AllPosts'));
-
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <Suspense fallback={<Load />}>
-            <Home />
-          </Suspense>
-        </Route>
-        <Route path="/all-posts" exact>
-          <Suspense fallback={<Load />}>
-            <AllPosts />
-          </Suspense>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+		<h1>Home</h1>
   );
 }
 
